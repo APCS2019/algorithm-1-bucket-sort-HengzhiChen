840400f217d74f1b4ed229195d11b38d532cab86
HengzhiChen <45375761+HengzhiChen@users.noreply.github.com>
2018-12-18 08:40:10 +0800

    Update BucketSort.java

--- BucketSort.java
+++ BucketSort.java
@@ -4,17 +4,17 @@
 4.输出b里不为0的
 a b
     int b = a[0];
-    for( int i = 0;i<a.length()-1;i++){
+    for( int i = 0;i<a.length-1;i++){
         if(a[i+1] > a[i]){
           b = a[i+1];
         }
     }
-    int[] c = new Integar(b.length()+1);   456456
-    for( int i = 0;i<a.length();i++){
+    int[] c = new Integar(b.length+1);   456456
+    for( int i = 0;i<a.length;i++){
         int f = a[i];
         c[f] += 1;
     }
-    for(int i = 0;i<a.length();i++){
+    for(int i = 0;i<a.length;i++){
         if(c[i]!=0){
           System.out.print(i);
         }
